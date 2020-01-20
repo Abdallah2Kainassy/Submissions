582efd51fc05252750107d08cfbc819907c48542
Abdallah2Kainassy <abdallahkmainassy@gmail.com>
2020-01-20 11:40:47 +0200

    Basic Queries

---
+++ Exercises/SQLite Basic/answers.sh
@@ -0,0 +1,15 @@
+1.select  name from students;
+2.select  * from students where age>30 ;
+3.select  name from students where age>30 and gender='F';
+4.select points from students where ID=1;
+5.INSERT INTO students ( Name, Age,Gender,Points)
+VALUES ( 'Abdallah',30,'M', 200);
+6.UPDATE students
+SET Points=350
+WHERE ID=2;
+7.UPDATE students
+SET Points=150
+WHERE ID=1;
+
+
+

7b97c1533d1da77f1acc9786e16fd92ac8ca643d
Abdallah2Kainassy <abdallahkmainassy@gmail.com>
2020-01-23 10:54:27 +0200

    complete the step 3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
-const http = require("http");
-const server = http.createServer((req, res) => {
-  res.end("OK");
+const server = express();
+let date_ob = new Date();
+let hours = date_ob.getHours();
+let minutes = date_ob.getMinutes();
+let time = hours + ":" + minutes;
+server.get("/test", (req, res) => {
+  res.json({
+    status: 200,
+    message: "ok"
+  });
 });
+server.get("/time", (req, res) => {
+  res.json({
+    status: 200,
+    message: time
+  });
+});
+
 server.listen(3000, "127.0.0.1", () => {
   console.log("server running .......");
 });
